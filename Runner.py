b8ab0f97faccece779a28dae277966f07ce6d936
123mjb <micahbutt@outlook.com>
2023-05-11 20:24:38 +0100

    more

--- Runner.py
+++ Runner.py
@@ -1,2 +1,6 @@
+from os import listdir
+from os.path import isfile, join
 def main():
-    print("")
+    file = input("What File Are You Running?")
+    onlyfiles = [f for f in listdir(".") if isfile(join(".", f))]
+    print(onlyfiles)
