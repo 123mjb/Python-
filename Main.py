1d6f8abcbf67e076d93e5be66f1a75c220c0f4d6
123mjb <micahbutt@outlook.com>
2023-05-11 19:18:30 +0100

    Start

---
+++ Main.py
@@ -0,0 +1,18 @@
+from Runner import main as run
+from SpeedTester import main as test
+
+switcher = {
+    "A""a":True,
+    "B""b":False,
+}
+while True:
+    print("What Are You Doing:\n- A - Running a file\n- B - Testing how long a file takes to run")
+    inp = input()
+    if inp in switcher.keys and switcher[inp]:
+        run()
+    elif inp in switcher.keys:
+        test()
+    else:
+        print("Input A or B")
+    
+    # [switcher[inp] if inp in switcher.keys else print("Input A or B")]
