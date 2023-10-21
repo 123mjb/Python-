895fcc7f686e39383cd22cf8d1bfd79618a0c233
123mjb <75265969+123mjb@users.noreply.github.com>
2023-10-21 13:33:41 +0000

    kinda works

--- runner.py
+++ runner.py
@@ -1,10 +1,20 @@
 """For users to run a python file"""
-from os import listdir
+from os import listdir, system
 from os.path import isfile, join
 def main():
     '''Interacts with the user to find what file they want to run.'''
-    
     fsysprnt()
+    path = ''
+    while True:
+        print("Where do you want to go / run python file.")
+        inp = input()
+        if isfile(('.' if path == '' else path)+inp):
+            system('python '+('.' if path == '' else path)+inp)
+            break
+        path+=inp+"/"
+        print(further(path,0))
+            
+    
 def fsysprnt():
     '''Prints the structure of the file system that the file is run in.'''
     layout = ""
@@ -13,7 +23,7 @@ def fsysprnt():
             layout += i + "\n"
         else:
             layout += i + "/\n"
-            layout += further(join(".",i),1)
+            layout += further(join(".",i+"/"),1)
     print(layout)
 def further(a : str,b : int):
     '''
@@ -34,11 +44,11 @@ def further(a : str,b : int):
           
     '''
     layout = ""
-    for i in listdir(a+"/"):
-        if isfile(join(a+"/",i)):
+    for i in listdir(a):
+        if isfile(join(a,i)):
             layout += ((b*3)*" ") + i + "\n"
         else:
             layout += ((b*3)*" ") + i + "/\n"
-            layout += further(join(a+"/",i),1 + b)
+            layout += further(join(a,i+"/"),1 + b)
     return layout
 main()
