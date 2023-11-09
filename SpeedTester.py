41f8559308d940ecadd239ab705f2e0feecd090e
123mjb <75265969+123mjb@users.noreply.github.com>
2023-11-09 08:26:17 +0000

    Making SpeedTester

--- SpeedTester.py
+++ SpeedTester.py
@@ -1,2 +1,15 @@
+import time
+from os import system
+from os.path import isfile
+from runner import fsysprnt, further
 def main():
-    print("2")
+    '''Interacts with the user to find what file they want to run.'''
+    fsysprnt()
+    path = ''
+    while True:
+        print("Where do you want to go / run python file.")
+        inp = input()
+        if isfile(('.' if path == '' else path)+inp):
+            break
+        path+=inp+"/"
+        print(further(path,0))
