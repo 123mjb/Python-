1b4db34361e2acd970464282b67bb607fc117dfc
123mjb <75265969+123mjb@users.noreply.github.com>
2023-11-15 08:26:07 +0000

    LetterAndPlaceWeighter

--- NewWordleSolver/LetterAndPlaceWeighter.py
+++ NewWordleSolver/LetterAndPlaceWeighter.py
@@ -8,7 +8,12 @@ class Generate:
             5 : {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
         }
         self.letters = {"a":1,"b":2,"c":3,"d":4,"e":4,"f":4,"g":4,"h":4,"i":4,"j":4,"k":4,"l":4,"m":4,"n":4,"o":4,"p":4,"q":4,"r":4,"s":4,"t":4,"u":4,"v":4}
-        pass    
-    def values_create():
+        self.allwords = [""]
+    def values_create(self):
         with open("NewWordleSolver/Answers.txt","r",encoding="utf-8") as f:
-            
+            self.allwords = f.readlines()
+        for i in self.allwords:
+            self.individual_values(i)
+    def individual_values(self,word):
+        for i in range(0,len(word)):
+            self.values[i][self.letters[word[i]]] += 1
